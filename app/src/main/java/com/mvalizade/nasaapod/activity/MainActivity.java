0d1cda58cd2939e44adec0d3de31dba68d196cd9
mohamad valizade <valizade.mohamad@gmail.com>
2018-03-09 14:31:23 +0330

    optimize sliding menu.

--- app/src/main/java/com/mvalizade/nasaapod/activity/MainActivity.java
+++ app/src/main/java/com/mvalizade/nasaapod/activity/MainActivity.java
@@ -1,9 +1,7 @@
 package com.mvalizade.nasaapod.activity;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,13 +11,11 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
@@ -59,33 +55,11 @@ public class MainActivity extends MAppCompatActivity {
   private View navHeader;
   private ImageView imgNavHeaderBg, imgProfile;
   private TextView txtName, txtWebsite;
-  private Toolbar toolbar;
-  private FloatingActionButton fab;
 
   // urls to load navigation header background image
   // and profile image
   private static final String urlNavHeaderBg = "https://api.androidhive.info/images/nav-menu-header-bg.jpg";
   private static final String urlProfileImg = "https://lh3.googleusercontent.com/eCtE_G34M9ygdkmOpYvCag1vBARCmZwnVS6rS5t4JLzJ6QgQSBquM0nuTsCpLhYbKljoyS-txg";
-
-  // index to identify current nav menu item
-  public static int navItemIndex = 0;
-
-  // tags used to attach the fragments
-  private static final String TAG_HOME = "home";
-  private static final String TAG_PHOTOS = "photos";
-  private static final String TAG_MOVIES = "movies";
-  private static final String TAG_NOTIFICATIONS = "notifications";
-  private static final String TAG_SETTINGS = "settings";
-  public static String CURRENT_TAG = TAG_HOME;
-
-  // toolbar titles respected to selected nav menu item
-  private String[] activityTitles;
-
-  // flag to load home fragment when user presses back key
-  private boolean shouldLoadHomeFragOnBackPress = true;
-  private Handler mHandler;
-
-
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -114,6 +88,25 @@ public class MainActivity extends MAppCompatActivity {
 
     //getImage(Base.API_STATE_LIST_IMAGE);
 
+
+
+
+
+    drawer = findViewById(R.id.drawer_layout);
+    navigationView = findViewById(R.id.nav_view);
+
+    // Navigation view header
+    navHeader = navigationView.getHeaderView(0);
+    txtName = navHeader.findViewById(R.id.name);
+    txtWebsite = navHeader.findViewById(R.id.website);
+    imgNavHeaderBg = navHeader.findViewById(R.id.img_header_bg);
+    imgProfile = navHeader.findViewById(R.id.img_profile);
+
+    // load nav menu header data
+    loadNavHeader();
+
+    // initializing navigation menu
+    setUpNavigationView();
   }
 
   private void getImage(final int state) {
@@ -219,73 +212,8 @@ public class MainActivity extends MAppCompatActivity {
     Glide.with(this).load(urlProfileImg)
       .transition(new DrawableTransitionOptions().crossFade())
       .thumbnail(0.5f)
-      .apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL).bitmapTransform(new CircleTransform(this)))
+      .apply(RequestOptions.bitmapTransform(new CircleTransform(this)))
       .into(imgProfile);
-
-    // showing dot next to notifications label
-    navigationView.getMenu().getItem(3).setActionView(R.layout.menu_dot);
-  }
-
-  /***
-   * Returns respected fragment that user
-   * selected from navigation menu
-   */
-  private void loadHomeFragment() {
-    // selecting appropriate nav menu item
-    selectNavMenu();
-
-    // set toolbar title
-    setToolbarTitle();
-
-    // if user select the current navigation menu again, don't do anything
-    // just close the navigation drawer
-    if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
-      drawer.closeDrawers();
-
-      // show or hide the fab button
-      toggleFab();
-      return;
-    }
-
-    // Sometimes, when fragment has huge data, screen seems hanging
-    // when switching between navigation menus
-    // So using runnable, the fragment is loaded with cross fade effect
-    // This effect can be seen in GMail app
-    /*Runnable mPendingRunnable = new Runnable() {
-      @Override
-      public void run() {
-        // update the main content by replacing fragments
-        Fragment fragment = getHomeFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
-          android.R.anim.fade_out);
-        fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
-        fragmentTransaction.commitAllowingStateLoss();
-      }
-    };*/
-
-    // If mPendingRunnable is not null, then add to the message queue
-    /*if (mPendingRunnable != null) {
-      mHandler.post(mPendingRunnable);
-    }*/
-
-    // show or hide the fab button
-    toggleFab();
-
-    //Closing drawer on item click
-    drawer.closeDrawers();
-
-    // refresh toolbar menu
-    invalidateOptionsMenu();
-  }
-
-
-  private void setToolbarTitle() {
-    getSupportActionBar().setTitle(activityTitles[navItemIndex]);
-  }
-
-  private void selectNavMenu() {
-    navigationView.getMenu().getItem(navItemIndex).setChecked(true);
   }
 
   private void setUpNavigationView() {
@@ -294,31 +222,36 @@ public class MainActivity extends MAppCompatActivity {
 
       // This method will trigger on item Click of navigation menu
       @Override
-      public boolean onNavigationItemSelected(MenuItem menuItem) {
+      public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
         //Check to see which item was being clicked and perform appropriate action
         switch (menuItem.getItemId()) {
           //Replacing the main content with ContentFragment Which is our Inbox View;
           case R.id.nav_home:
-            navItemIndex = 0;
-            CURRENT_TAG = TAG_HOME;
-            break;
+            // launch new intent instead of loading fragment
+            //startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
+            drawer.closeDrawers();
+            return true;
           case R.id.nav_photos:
-            navItemIndex = 1;
-            CURRENT_TAG = TAG_PHOTOS;
-            break;
+            // launch new intent instead of loading fragment
+            //startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
+            drawer.closeDrawers();
+            return true;
           case R.id.nav_movies:
-            navItemIndex = 2;
-            CURRENT_TAG = TAG_MOVIES;
-            break;
+            // launch new intent instead of loading fragment
+            //startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
+            drawer.closeDrawers();
+            return true;
           case R.id.nav_notifications:
-            navItemIndex = 3;
-            CURRENT_TAG = TAG_NOTIFICATIONS;
-            break;
+            // launch new intent instead of loading fragment
+            //startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
+            drawer.closeDrawers();
+            return true;
           case R.id.nav_settings:
-            navItemIndex = 4;
-            CURRENT_TAG = TAG_SETTINGS;
-            break;
+            // launch new intent instead of loading fragment
+            //startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
+            drawer.closeDrawers();
+            return true;
           case R.id.nav_about_us:
             // launch new intent instead of loading fragment
             //startActivity(new Intent(MainActivity.this, AboutUsActivity.class));
@@ -329,26 +262,13 @@ public class MainActivity extends MAppCompatActivity {
             //startActivity(new Intent(MainActivity.this, PrivacyPolicyActivity.class));
             drawer.closeDrawers();
             return true;
-          default:
-            navItemIndex = 0;
         }
-
-        //Checking if the item is in checked state or not, if not make it in checked state
-        if (menuItem.isChecked()) {
-          menuItem.setChecked(false);
-        } else {
-          menuItem.setChecked(true);
-        }
-        menuItem.setChecked(true);
-
-        loadHomeFragment();
-
         return true;
       }
     });
 
 
-    ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+    ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, R.string.openDrawer, R.string.closeDrawer) {
 
       @Override
       public void onDrawerClosed(View drawerView) {
@@ -363,9 +283,6 @@ public class MainActivity extends MAppCompatActivity {
       }
     };
 
-    //Setting the actionbarToggle to drawer layout
-    drawer.setDrawerListener(actionBarDrawerToggle);
-
     //calling sync state is necessary or else your hamburger icon wont show up
     actionBarDrawerToggle.syncState();
   }
@@ -376,72 +293,7 @@ public class MainActivity extends MAppCompatActivity {
       drawer.closeDrawers();
       return;
     }
-
-    // This code loads home fragment when back key is pressed
-    // when user is in other fragment than home
-    if (shouldLoadHomeFragOnBackPress) {
-      // checking if user is on other navigation menu
-      // rather than home
-      if (navItemIndex != 0) {
-        navItemIndex = 0;
-        CURRENT_TAG = TAG_HOME;
-        loadHomeFragment();
-        return;
-      }
-    }
-
     super.onBackPressed();
   }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-
-    // show menu only when home fragment is selected
-    if (navItemIndex == 0) {
-      getMenuInflater().inflate(R.menu.mian, menu);
-    }
-
-    // when fragment is notifications, load the menu created for notifications
-    if (navItemIndex == 3) {
-      getMenuInflater().inflate(R.menu.notification, menu);
-    }
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_logout) {
-      Toast.makeText(getApplicationContext(), "Logout user!", Toast.LENGTH_LONG).show();
-      return true;
-    }
-
-    // user is in notifications fragment
-    // and selected 'Mark all as Read'
-    if (id == R.id.action_mark_all_read) {
-      Toast.makeText(getApplicationContext(), "All notifications marked as read!", Toast.LENGTH_LONG).show();
-    }
-
-    // user is in notifications fragment
-    // and selected 'Clear All'
-    if (id == R.id.action_clear_notifications) {
-      Toast.makeText(getApplicationContext(), "Clear all notifications!", Toast.LENGTH_LONG).show();
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
-
-  // show or hide the fab
-  private void toggleFab() {
-    if (navItemIndex == 0)
-      fab.show();
-    else
-      fab.hide();
-  }
 }
+
