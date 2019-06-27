45fb4ddb6bc3b10492c5736f202cc77f89522bc0
Mohamad Valizade <valizade.mohamad@gmail.com>
2019-06-27 15:34:47 +0430

    Apply RxJava for communication with server with retrofit.

--- app/src/main/java/com/valizade/hellouniverse/api/ApodClient.java
+++ app/src/main/java/com/valizade/hellouniverse/api/ApodClient.java
@@ -1,24 +1,26 @@
 package com.valizade.hellouniverse.api;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApodClient {
 
-  public static final String BASE_URL="https://api.nasa.gov/";
+  public static final String BASE_URL = "https://api.nasa.gov/";
   private static Retrofit retrofit = null;
 
-  public static Retrofit getClient(){
-    if(retrofit == null) {
+  public static Retrofit getClient() {
+    if (retrofit == null) {
       retrofit = new Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build();
+          .baseUrl(BASE_URL)
+          .addConverterFactory(GsonConverterFactory.create())
+          .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+          .build();
     }
     return retrofit;
   }
 
-  public ApodService getApodService(){
+  public ApodService getApodService() {
     return getClient().create(ApodService.class);
   }
 }
